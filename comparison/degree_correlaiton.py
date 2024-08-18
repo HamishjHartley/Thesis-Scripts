b86d2693ff21e98c6a22b09be677c972472e7061
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-08-18 13:26:44 +0100

    Degree correlation matrix code

--- comparison/degree_correlaiton.py
+++ comparison/degree_correlaiton.py
@@ -1,29 +1,103 @@
 import networkx as nx
 import numpy as np
-from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
 
-# Create two random graphs
-G1 = nx.erdos_renyi_graph(23, 0.1) # Set to 23 nodes to match janet_backbone
-G2 = nx.barabasi_albert_graph(100, 5)
+#Load in graphs for comparison
+topo_path = 'C:/Users/theha/OneDrive/Desktop/Masters Project/Topology Set/'
+ER1_6 = nx.read_gml(topo_path+'ER1_6.gml')
+ER2_6 = nx.read_gml(topo_path+'ER2_6.gml')
 
+BA1_6 = nx.read_gml(topo_path+'BA1_6.gml')
+BA2_6 = nx.read_gml(topo_path+'BA2_6.gml')
 
-# Import real-world graph
-geant_file = 'C:/Users/theha/OneDrive/Desktop/Masters Project/Scripts/topology zoo/Geant2012.gml'
-janet_backbone = 'C:/Users/theha/OneDrive/Desktop/Masters Project/Scripts/topology zoo/Janetbackbone.gml'
+ER1_13 = nx.read_gml(topo_path+'ER1_13.gml')
+ER2_13 = nx.read_gml(topo_path+'ER2_13.gml')
 
+BA1_13 = nx.read_gml(topo_path+'BA1_13.gml')
+BA2_13 = nx.read_gml(topo_path+'BA2_13.gml')
 
-janet_backbone = nx.read_gml(janet_backbone)
+ER1_20 = nx.read_gml(topo_path+'ER1_20.gml')
+ER2_20 = nx.read_gml(topo_path+'ER2_20.gml')
 
-# Get the degree sequences
+BA1_20 = nx.read_gml(topo_path+'BA1_20.gml')
+BA2_20 = nx.read_gml(topo_path+'BA2_20.gml')
 
-# Random graph
-degree_seq_G1 = [d for n, d in G1.degree()]
-degree_seq_G2 = [d for n, d in G2.degree()]
+ER1_23 = nx.read_gml(topo_path+'ER1_23.gml')
+ER2_23 = nx.read_gml(topo_path+'ER2_23.gml')
 
+BA1_23 = nx.read_gml(topo_path+'BA1_23.gml')
+BA2_23 = nx.read_gml(topo_path+'BA2_23.gml')
+
+ER1_24 = nx.read_gml(topo_path+'ER1_24.gml')
+ER2_24 = nx.read_gml(topo_path+'ER2_24.gml')
+
+BA1_24 = nx.read_gml(topo_path+'BA1_24.gml')
+BA2_24 = nx.read_gml(topo_path+'BA2_24.gml')
+
+ER1_27 = nx.read_gml(topo_path+'ER1_27.gml')
+ER2_27 = nx.read_gml(topo_path+'ER2_27.gml')
+
+BA1_27 = nx.read_gml(topo_path+'BA1_27.gml')
+BA2_27 = nx.read_gml(topo_path+'BA2_27.gml')
 # Real-world graph
-degree_seq_GEANT = [d for n, d in janet_backbone.degree()]
+path = 'C:/Users/theha/OneDrive/Documents/GitHub/3D-internet-zoo/dataset/'
+# 6 nodes
+Dataxchange = nx.read_gml(path+"Dataxchange.gml")
+Layer42 = nx.read_gml(path+"Layer42.gml")
 
-# Calculate Pearson correlation between the degree sequences
-correlation, _ = pearsonr(degree_seq_G1, janet_backbone)
+# 13 Nodes
+Navigata = nx.read_gml(path+"Navigata.gml")
+Nsfnet = nx.read_gml(path+"Nsfnet.gml")
 
-print(f"Degree sequence correlation: {correlation}")
+# 20 nodes
+Quest = nx.read_gml(path+"Quest.gml")
+Eli = nx.read_gml(path+"EliBackbone.gml")
+
+# 23 nodes
+York = nx.read_gml(path+"York.gml")
+Aconet = nx.read_gml(path+"Aconet.gml")
+
+# 24 nodes
+Psinet = nx.read_gml(path+"Psinet.gml")
+Vision = nx.read_gml(path+"VisionNet.gml")
+
+# 27 nodes
+Bbnplanet = nx.read_gml(path+"Bbnplanet.gml")
+Integra = nx.read_gml(path+"Integra.gml")
+
+def degree_correlation_matrix(G):
+    # Get the degree of each node
+    degrees = dict(G.degree())
+    
+    # Get the maximum degree to define the size of the matrix
+    max_degree = max(degrees.values())
+    
+    # Initialize a matrix of zeros with dimensions (max_degree+1) x (max_degree+1)
+    matrix = np.zeros((max_degree+1, max_degree+1), dtype=int)
+    
+    # Fill in the matrix with degree correlations
+    for u, v in G.edges():
+        deg_u = degrees[u]
+        deg_v = degrees[v]
+        matrix[deg_u][deg_v] += 1
+        if deg_u != deg_v:  # Ensure symmetry
+            matrix[deg_v][deg_u] += 1
+    
+    return matrix
+
+def plot_degree_correlation_matrix(matrix, label):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(matrix, origin='lower', cmap='Blues')
+    plt.colorbar(label='Number of Edges')
+    plt.xlabel('Degree of Node u')
+    plt.ylabel('Degree of Node v')
+    plt.title(f'Degree Correlation Matrix: {label}')
+    plt.grid(False)
+    plt.show()
+
+
+# Calculate the degree correlation matrix
+matrix = degree_correlation_matrix(Bbnplanet)
+
+# Plot the degree correlation matrix
+plot_degree_correlation_matrix(matrix, 'ER1_27')
--- comparison/shortest_path.py
+++ comparison/shortest_path.py
@@ -26,6 +26,51 @@ def plot_distribution(lengths1, lengths2, lengths3, label1, label2, label3):
     plt.legend()
     plt.show()
 
+def plot_distribution_6(lengths1, lengths2, lengths3, lengths4, lengths5, lengths6, label1, label2, label3, label4, label5, label6):
+    # Plot histograms for the six distributions
+    max_length = max(lengths1 + lengths2 + lengths3 + lengths4 + lengths5 + lengths6)
+    bins = range(max_length + 1)
+    
+    plt.hist(lengths1, bins=bins, alpha=0.5, label=label1, density=True, color="Blue")
+    plt.hist(lengths2, bins=bins, alpha=0.5, label=label2, density=True, color="Red")
+    plt.hist(lengths3, bins=bins, alpha=0.5, label=label3, density=True, color="Green")
+    plt.hist(lengths4, bins=bins, alpha=0.5, label=label4, density=True, color="Purple")
+    plt.hist(lengths5, bins=bins, alpha=0.5, label=label5, density=True, color="Orange")
+    plt.hist(lengths6, bins=bins, alpha=0.5, label=label6, density=True, color="Cyan")
+    
+    plt.xlabel('Shortest Path Length')
+    plt.ylabel('Frequency')
+    plt.title('Shortest Path Length Distribution')
+    plt.legend()
+    plt.show()
+
+def plot_line_distribution(lengths1, lengths2, lengths3, lengths4, lengths5, lengths6, label1, label2, label3, label4, label5, label6):
+    # Compute frequency distributions
+    max_length = max(lengths1 + lengths2 + lengths3 + lengths4 + lengths5 + lengths6)
+    bins = np.arange(0, max_length + 1)
+    
+    freq1, _ = np.histogram(lengths1, bins=bins, density=True)
+    freq2, _ = np.histogram(lengths2, bins=bins, density=True)
+    freq3, _ = np.histogram(lengths3, bins=bins, density=True)
+    freq4, _ = np.histogram(lengths4, bins=bins, density=True)
+    freq5, _ = np.histogram(lengths5, bins=bins, density=True)
+    freq6, _ = np.histogram(lengths6, bins=bins, density=True)
+
+    # Plot line distributions
+    plt.plot(bins[:-1], freq1, label=label1, linestyle='-', color='blue')
+    plt.plot(bins[:-1], freq2, label=label2, linestyle='-', color='red')
+    plt.plot(bins[:-1], freq3, label=label3, linestyle='-', color='green')
+    plt.plot(bins[:-1], freq4, label=label4, linestyle='-', color='purple')
+    plt.plot(bins[:-1], freq5, label=label5, linestyle='-', color='orange')
+    plt.plot(bins[:-1], freq6, label=label6, linestyle='-', color='cyan')
+    
+    plt.xlabel('Shortest Path Length')
+    plt.ylabel('Frequency')
+    plt.title('Shortest Path Length Distribution Comparison')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 
 #Load in graphs for comparison
 topo_path = 'C:/Users/theha/OneDrive/Desktop/Masters Project/Topology Set/'
@@ -92,13 +137,22 @@ Integra = nx.read_gml(path+"Integra.gml")
 
 
 # Compute shortest path length distributions
-lengths1 = shortest_path_length_distribution(BA1_27)
-lengths2 = shortest_path_length_distribution(ER2_27)
-lengths3 = shortest_path_length_distribution(Integra)
+lengths1 = shortest_path_length_distribution(ER1_6)
+lengths2 = shortest_path_length_distribution(ER2_6)
+
+lengths3 = shortest_path_length_distribution(BA1_27)
+lengths4 = shortest_path_length_distribution(BA2_27)
+
+lengths5 = shortest_path_length_distribution(Dataxchange)
+lengths6 = shortest_path_length_distribution(Integra)
 
 # Plot and compare the distributions
-plot_distribution(lengths1, lengths2, lengths3,'G1 (Erdos-Renyi)', 'G2 (Barabasi-Albert)', 'G3 (Vision)')
+plot_distribution(lengths1,lengths2,lengths5, 'Erdos-Renyi, 6 nodes, p=0.3', 'Erdos-Renyi, 6 nodes, p=0.5', 'Dataxchange')
 
+#plot_distribution_6(lengths1, lengths2, lengths3, lengths4, lengths5, lengths6,'ER1_6', 'BA2_6', 'ER1_13', 'BA2_13', 'ER1_20', 'BA2_20')
+
+
+#plot_line_distribution(lengths1, lengths2, lengths3, lengths4, lengths5, lengths6, 'Erdos-Renyi, 27 nodes, p=0.3', 'Erdos-Renyi, 27 nodes, p=0.5', 'Barabasi-Albert, 27 nodes, m=2', 'Barabasi-Albert, 27 nodes, m=3', 'Bbnplanet', 'Integra')
 
 #ks_stat, p_value = ks_2samp(lengths1, lengths2)
 #print(f"KS Statistic: {ks_stat}, p-value: {p_value}")
